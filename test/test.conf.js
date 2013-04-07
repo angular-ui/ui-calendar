c849bddf598e78fa9bdaf4343abf14cad07c57de
Josh Kurz <jkurz25@gmail.com>
2013-04-07 11:33:40 -0400

    modified for bower #1

--- test/test.conf.js
+++ test/test.conf.js
@@ -2,11 +2,11 @@ basePath = '..';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'components/jquery.js',
-  'components/jquery-ui.js',
-  'components/angular.js',
-  'components/angular-mocks.js',
-  'components/fullcalendar.js',
+  'components/jquery/jquery.js',
+  'components/jquery-ui/jquery-ui.js',
+  'components/angular/angular.js',
+  'components/angular-mocks/angular-mocks.js',
+  'components/fullcalendar/fullcalendar.js',
   'src/calendar.js',
   'test/*.spec.js'
 ];
