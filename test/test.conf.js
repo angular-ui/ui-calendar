04c7bce28cbba95475da4e3144e5170b79f9df62
joshkurz <jkurz25@gmail.com>
2013-07-05 22:15:47 -0400

    test(gruntConfig) updated file paths for new bower path
    Signed-off-by: joshkurz <jkurz25@gmail.com>

--- test/test.conf.js
+++ test/test.conf.js
@@ -2,11 +2,11 @@ basePath = '..';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'components/jquery/jquery.js',
-  'components/jquery-ui/jquery-ui.js',
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'components/fullcalendar/fullcalendar.js',
+  'bower_components/jquery/jquery.js',
+  'bower_components/jquery-ui/jquery-ui.js',
+  'bower_components/angular/angular.js',
+  'bower_components/angular-mocks/angular-mocks.js',
+  'bower_components/fullcalendar/fullcalendar.js',
   'src/calendar.js',
   'test/*.spec.js'
 ];
