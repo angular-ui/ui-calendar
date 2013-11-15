97d9bf72288779c508264139b3b4d21aede43eab
Josh Kurz <josh.kurz@turner.com>
2013-11-14 23:38:00 -0500

    tests(karma) updated karma to 0.10.4 and fixed the test.conf.js file to match the updates for karma
    Signed-off-by: Josh Kurz <josh.kurz@turner.com>

--- test/test.conf.js
+++ test/test.conf.js
@@ -1,24 +1,30 @@
-basePath = '..';
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    'bower_components/jquery/jquery.js',
-    'bower_components/jquery-ui/ui/*.js',
-    'bower_components/angular/angular.js',
-    'bower_components/angular-mocks/angular-mocks.js',
-    'bower_components/fullcalendar/fullcalendar.js',
-    'src/calendar.js',
-    'test/*.spec.js'
-];
+// Contents of: config/karma.conf.js
+module.exports = function (config) {
+  config.set({
+    basePath : '..',
+    files : [
+        'bower_components/jquery/jquery.js',
+        'bower_components/jquery-ui/ui/*.js',
+        'bower_components/angular/angular.js',
+        'bower_components/angular-mocks/angular-mocks.js',
+        'bower_components/fullcalendar/fullcalendar.js',
+        'src/calendar.js',
+        'test/*.spec.js'
+    ],
 
-singleRun = true;
+    singleRun : true,
 
-browsers = ['Chrome'];
+    browsers : ['Chrome'],
 
-preprocessors = {
-    '**/src/*.js': 'coverage'
+    frameworks : ["jasmine"],
+
+    preprocessors : {
+        '**/src/*.js': 'coverage'
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit'
+    reporters : ['progress', 'coverage']
+
+  });
 };
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress', 'coverage'];
