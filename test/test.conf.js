ed475e9a9ef91a07a428fa44d1e06c39939a7ee4
Uri Goldshtein <uri.goldshtein@gmail.com>
2014-08-07 19:48:52 +0200

    feat(v2): Remove redundant dependencies that arrive from fullcalendar

--- test/test.conf.js
+++ test/test.conf.js
@@ -4,7 +4,6 @@ module.exports = function (config) {
     basePath : '..',
     files : [
         'bower_components/jquery/dist/jquery.js',
-        'bower_components/jquery-ui/ui/*.js',
         'bower_components/angular/angular.js',
         'bower_components/angular-mocks/angular-mocks.js',
         'bower_components/moment/moment.js',
