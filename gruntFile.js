97f6faa6d6a33fabb7235ea0a2045ccbc7a72617
Chaitanya Kumar Kamatham <chaitanya@captora.com>
2015-06-25 13:49:11 -0700

    Added Support For Angular 1.4.1

--- gruntFile.js
+++ gruntFile.js
@@ -1,49 +1,65 @@
 module.exports = function (grunt) {
-  "use strict";
+    "use strict";
 
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'karma']);
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'karma']);
 
-  // uglify
-  grunt.registerTask('minify', ['uglify']);
+    // uglify
+    grunt.registerTask('minify', ['uglify']);
 
-  var testConfig = function(configFile, customOptions) {
-    var options = { configFile: configFile, keepalive: true };
-    var travisOptions = process.env.TRAVIS && { browsers: ['Firefox'], reporters: 'dots' };
-    return grunt.util._.extend(options, customOptions, travisOptions);
-  };
+    grunt.registerTask('serve', ['connect']);
 
-  // Project configuration.
-  grunt.initConfig({
-    karma: {
-      unit: {
-        options: testConfig('test/test.conf.js')
-      }
-    },
-    jshint:{
-      files:['src/**/*.js', 'test/**/*.js', 'demo/**/*.js'],
-      options:{
-        curly:true,
-        eqeqeq:true,
-        immed:true,
-        latedef:true,
-        newcap:true,
-        noarg:true,
-        sub:true,
-        boss:true,
-        eqnull:true,
-        globals:{}
-      }
-    },
-    uglify: {
-      build: {
-        src: ['src/**/*.js'],
-        dest: 'calendar.min.js'
-      }
-    }
-  });
+    var testConfig = function (configFile, customOptions) {
+        var options = {configFile: configFile, keepalive: true};
+        var travisOptions = process.env.TRAVIS && {browsers: ['Firefox'], reporters: 'dots'};
+        return grunt.util._.extend(options, customOptions, travisOptions);
+    };
+
+    // Project configuration.
+    grunt.initConfig(
+        {
+            karma: {
+                unit: {
+                    options: testConfig('test/test.conf.js')
+                }
+            },
+            jshint: {
+                files: ['src/**/*.js', 'test/**/*.js', 'demo/**/*.js'],
+                options: {
+                    curly: true,
+                    eqeqeq: true,
+                    immed: true,
+                    latedef: true,
+                    newcap: true,
+                    noarg: true,
+                    sub: true,
+                    boss: true,
+                    eqnull: true,
+                    globals: {}
+                }
+            },
+            uglify: {
+                build: {
+                    src: ['src/**/*.js'],
+                    dest: 'calendar.min.js'
+                }
+            },
+            connect: {
+                server: {
+                    options: {
+                        port: 8000,
+                        open: true,
+                        debug: true,
+                        keepalive: true,
+                        hostname: '*',
+                        base: ['demo', '.']
+                    }
+                }
+            }
+        }
+    );
 
 };
