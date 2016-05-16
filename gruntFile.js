4314b46722df9a18f3b539be3f8f86eb24711e3d
Peter Vermeulen <nmb.peterv@gmail.com>
2016-05-17 00:36:39 +0200

    move jshint config to jshintrc file and start refactor for eslint

--- gruntFile.js
+++ gruntFile.js
@@ -1,63 +1,59 @@
 module.exports = function (grunt) {
-  "use strict";
+    "use strict";
 
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'karma']);
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'karma']);
 
-  // uglify
-  grunt.registerTask('minify', ['uglify']);
-  //connect - local server 
-  grunt.registerTask('serve', ['connect']);
+    // uglify
+    grunt.registerTask('minify', ['uglify']);
+    
+    //connect - local server 
+    grunt.registerTask('serve', ['connect']);
 
-  var testConfig = function(configFile, customOptions) {
-    var options = { configFile: configFile, keepalive: true };
-    var travisOptions = process.env.TRAVIS && { browsers: ['Firefox'], reporters: 'dots' };
-    return grunt.util._.extend(options, customOptions, travisOptions);
-  };
+    var testConfig = function (configFile, customOptions) {
+        var options = {
+            configFile : configFile,
+            keepalive : true
+        };
+        var travisOptions = process.env.TRAVIS && {
+            browsers : ['Firefox'],
+            reporters : 'dots'
+        };
+        return grunt.util._.extend(options, customOptions, travisOptions);
+    };
 
-  // Project configuration.
-  grunt.initConfig({
-    karma: {
-      unit: {
-        options: testConfig('test/test.conf.js')
-      }
-    },
-    jshint: {
-      files: ['src/**/*.js', 'test/**/*.js', 'demo/**/*.js'],
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: 'nofunc',
-        newcap: true,
-        noarg: true,
-        sub: true,
-        boss: true,
-        eqnull: true,
-        globals: {}
-      }
-    },
-    uglify: {
-      build: {
-        src: ['src/**/*.js'],
-        dest: 'calendar.min.js'
-      }
-    },
-    connect: {
-      server: {
-        options: {
-          port: 8000,
-            open: true,
-            debug: true,
-            keepalive: true,
-            hostname: '*',
-            base: ['demo', '.']
-          }
+    // Project configuration.
+    grunt.initConfig({
+        karma : {
+            unit : {
+                options : testConfig('test/test.conf.js')
+            }
+        },
+        jshint : {
+            files : ['src/**/*.js', 'test/**/*.js', 'demo/**/*.js'],
+            jshintrc : true
+        },
+        uglify : {
+            build : {
+                src : ['src/**/*.js'],
+                dest : 'calendar.min.js'
+            }
+        },
+        connect : {
+            server : {
+                options : {
+                    port : 8000,
+                    open : true,
+                    debug : true,
+                    keepalive : true,
+                    hostname : '*',
+                    base : ['demo', '.']
+                }
+            }
         }
-    }
-  });
+    });
 
 };
